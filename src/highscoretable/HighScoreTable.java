3aac96a0967da4fb842ae6cbb70cab13ef1b30f8
HUIJIA YU <hy91@duke.edu>
2016-04-03 21:05:45 -0400

    fixed comparator issues

--- src/highscoretable/HighScoreTable.java
+++ src/highscoretable/HighScoreTable.java
@@ -2,22 +2,25 @@ package highscoretable;
 
 import java.util.*;
 
+import javafx.scene.Node;
 import javafx.scene.layout.VBox;
 
 public class HighScoreTable extends VBox {
-	Comparator c;
+	Comparator comparator;
 
 	public HighScoreTable(HighScore... scores) {
 		super();
-		c = (o1, o2) -> ((HighScore) o1).getScore().compareTo(((HighScore) o2).getScore());
+		comparator = (o1, o2) -> ((HighScore) o1).getScore().compareTo(((HighScore) o2).getScore());
 
 		getChildren().addAll(Arrays.asList(scores));
-		getChildren().sort(c);
+		
+		getChildren().setAll(getChildren().sorted(comparator));
 	}
 
 	public void addScore(HighScore newscore) {
 		getChildren().add(newscore);
-		getChildren().sort(c);
+		getChildren().setAll(getChildren().sorted(comparator));
+
 	}
 
 }
