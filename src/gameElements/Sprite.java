75b0df75f5cea9363a44c61189d501894a7d61bf
mohabg <mg250@duke.edu>
2016-04-05 23:30:30 -0400

    Added Sprite Properties

--- src/gameElements/Sprite.java
+++ src/gameElements/Sprite.java
@@ -1,144 +1,19 @@
 package gameElements;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 
-import javafx.scene.Node;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.shape.Polygon;
-
-/**
- * @author Tavo Loaiza
- *	Basic game element
- */
-public class Sprite extends Observable {
-
-	private Node node;
-	private Vector vector;
-	private boolean visible;
-	private boolean movable;
-	private boolean colliable;
-
-	public Sprite() {
-		//Default init values
-		node = new Polygon();
-		vector = new Vector();
-		visible = false;
-		movable = false;
-		colliable = false;
+public class Sprite extends Observable{
+	private List<Behavior> behaviors;
+	private SpriteProperties myProperties;
+	
+	public Sprite(SpriteProperties properties){
+		behaviors = new ArrayList<>();
+		myProperties = properties;
 	}
-	public Sprite(Image image, double x, double y){
-		node = new ImageView(image);
-		this.visible = true;
-		vector = new Vector(x,y);
-		movable = true;
-		colliable = true;
+	public void setBehaviors(List<Behavior> behaviors){
+		this.behaviors = behaviors;
 	}
-
-	public Node getNode() {
-		return node;
-	}
-	public void setNode(Node node) {
-		this.node = node;
-	}
-
-	public void setImage(Image image) {
-		node = new ImageView(image);
-		propChange();
-	}
-
-	public Vector getVector() {
-		return vector;
-	}
-
-	public double getNextX(){
-		if(!movable)
-			return vector.getX();
-		return vector.getxVel();
-	}
-
-	public double getNextY(){
-		if(!movable)
-			return vector.getY();
-		return vector.getyVel();
-	}
-
-	public void setAngle(double angle){
-		vector.setAngle(angle);
-		if(node!=null)
-			node.setRotate(angle);
-		propChange();
-	}
-
-	public void setPosition(double x, double y){
-		vector.setCoord(x, y);
-		propChange();
-	}
-
-	public void setVelocity(double x, double y){
-		vector.setVelocity(x, y);
-		propChange();
-	}
-
-	public void setVector(Vector vector) {
-		this.vector = vector;
-		propChange();
-	}
-	public void setVector(double x, double y, double xVel, double yVel) {
-		vector.setCoord(x, y);
-		vector.setVelocity(xVel, yVel);
-		propChange();
-	}
-
-	public boolean isVisible() {
-		return visible;
-	}
-
-	public void setVisible(boolean visible) {
-		this.visible = visible;
-		node.setVisible(visible);
-		propChange();
-	}
-
-	public boolean isMoveable() {
-		return movable;
-	}
-
-	public void setMoveable(boolean moveable) {
-		this.movable = moveable;
-		propChange();
-	}
-
-	public boolean isColliable() {
-		return colliable;
-	}
-
-	public void setColliable(boolean colliable) {
-		this.colliable = colliable;
-		propChange();
-	}
-
-	public void updateNextPos(){
-		if(movable)
-			vector.setCoord(vector.getxVel(), vector.getyVel());
-		propChange();
-	}
-
-	public boolean intersects(Sprite other){
-		if(node!=null && other.node!=null)
-			return node.intersects(other.node.getBoundsInParent());
-		return false;
-	}
-
-	public boolean collides(Sprite other){
-		if(!colliable)
-			return false;
-		return intersects(other);
-	}
-
-	public void propChange(){
-		 setChanged();
-	     notifyObservers();
-	}
-
+	
 }
--- src/gameElements/SpriteProperties.java
+++ src/gameElements/SpriteProperties.java
@@ -1,34 +1,51 @@
 package gameElements;
 
 
-public class Vector {
+public class SpriteProperties {
 
 private double x;
 private double y;
+private double width;
+private double height;
 private double angle;
 private double xVel;
 private double yVel;
 
 
-	public Vector() {
+	public SpriteProperties() {
 		this.x = 0;
 		this.y = 0;
 		this.angle = 0;
 	}
 
-	public Vector(double x, double y, double angle, double xVel, double yVel){
+	public SpriteProperties(double x, double y, double angle, double xVel, double yVel){
 		this.x = x;
 		this.y = y;
 		this.angle = angle;
 		this.xVel = xVel;
 		this.yVel = yVel;
 	}
-	public Vector(double x, double y, double angle){
+	public SpriteProperties(double x, double y, double angle){
 		this.x = x;
 		this.y = y;
 		this.angle = angle;
 	}
+	
+	public double getWidth() {
+		return width;
+	}
 
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
 	public double getxVel() {
 		return xVel;
 	}
@@ -45,7 +62,7 @@ private double yVel;
 		this.yVel = yVel;
 	}
 
-	public Vector(double x, double y){
+	public SpriteProperties(double x, double y){
 		this(x, y, 0);
 	}
 
@@ -77,7 +94,7 @@ private double yVel;
 		return angle;
 	}
 
-	public double getDistance(Vector otherVect){
+	public double getDistance(SpriteProperties otherVect){
 		return Math.sqrt(( Math.pow(x,2) - Math.pow(otherVect.getX(),2))
 				+ (Math.pow(y,2) - Math.pow(otherVect.getY(), 2)));
 	}
