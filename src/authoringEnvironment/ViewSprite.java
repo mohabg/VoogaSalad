2c4203ccf1272dfa3fe6826cba176bb919bb680c
David Yan <davidyanduke@gmail.com>
2016-04-07 20:14:36 -0400

    Make better slider design

--- src/authoringEnvironment/ViewSprite.java
+++ src/authoringEnvironment/ViewSprite.java
@@ -3,38 +3,26 @@ package authoringEnvironment;
 /**
  * Created by davidyan on 4/4/16.
  */
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import spriteProperties.Health;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import settingsWindow.SettingsWindow;
-import spriteProperties.Attack;
-import spriteProperties.Defense;
-import spriteProperties.NumProperty;
-import java.util.*;
 
 public class ViewSprite extends ImageView {
 
     private String myRef;
-    private List<NumProperty> myPropertiesList;
-    private Health myHealth;
-    private Attack myAttack;
-    private Defense myDefense;
+//    private List<NumProperty> myPropertiesList;
+//    private Health myHealth;
+//    private Attack myAttack;
+//    private Defense myDefense;
 
     public ViewSprite() {
         super();
-        myHealth = new Health();
-        myAttack = new Attack();
-        myDefense = new Defense();
-        myPropertiesList = new ArrayList<>();
-        myPropertiesList.add(myHealth);
-        myPropertiesList.add(myAttack);
-        myPropertiesList.add(myDefense);
+//        myHealth = new Health();
+//        myAttack = new Attack();
+//        myDefense = new Defense();
+//        myPropertiesList = new ArrayList<>();
+//        myPropertiesList.add(myHealth);
+//        myPropertiesList.add(myAttack);
+//        myPropertiesList.add(myDefense);
     }
 
 
@@ -44,37 +32,37 @@ public class ViewSprite extends ImageView {
         setImage(image);
     }
 
-    public void setMyHealth(double test){
-        myHealth.setMyValue(test);
-    }
+//    public void setMyHealth(double test){
+//        myHealth.setMyValue(test);
+//    }
+//
+//    public double getMyHealth(){
+//        return myHealth.getMyValue();
+//    }
 
-    public double getMyHealth(){
-        return myHealth.getMyValue();
-    }
-
-    public void setSettingsContent(SettingsWindow mySettingsWindow){
-        VBox myBox = new VBox(8);
-        for(NumProperty aProp: myPropertiesList){
-            HBox myTempBox = new HBox();
-            Label myLabel = new Label(aProp.toString());
-            Slider mySlider = new Slider(0,100,aProp.getMyValue());
-//            mySlider.setMin(0);
-//            mySlider.setMax(100);
-            mySlider.setShowTickMarks(true);
-            mySlider.setShowTickLabels(true);
-
-            mySlider.valueProperty().addListener(new ChangeListener<Number>() {
-                public void changed(ObservableValue<? extends Number> ov,
-                                    Number old_val, Number new_val) {
-                    aProp.setMyValue((double) new_val);
-                }
-            });
-            myTempBox.getChildren().addAll(myLabel, mySlider);
-            myBox.getChildren().add(myTempBox);
-        }
-
-        mySettingsWindow.setContent(myBox);
-    }
+//    public void setSettingsContent(SettingsWindow mySettingsWindow){
+//        VBox myBox = new VBox(8);
+//        for(NumProperty aProp: myPropertiesList){
+//            HBox myTempBox = new HBox();
+//            Label myLabel = new Label(aProp.toString());
+//            Slider mySlider = new Slider(0,100,aProp.getMyValue());
+////            mySlider.setMin(0);
+////            mySlider.setMax(100);
+//            mySlider.setShowTickMarks(true);
+//            mySlider.setShowTickLabels(true);
+//
+//            mySlider.valueProperty().addListener(new ChangeListener<Number>() {
+//                public void changed(ObservableValue<? extends Number> ov,
+//                                    Number old_val, Number new_val) {
+//                    aProp.setMyValue((double) new_val);
+//                }
+//            });
+//            myTempBox.getChildren().addAll(myLabel, mySlider);
+//            myBox.getChildren().add(myTempBox);
+//        }
+//
+//        mySettingsWindow.setContent(myBox);
+//    }
 
     public String getMyImage(){
         return myRef;
@@ -84,9 +72,9 @@ public class ViewSprite extends ImageView {
 //        return imageview;
 //    }
 
-    public List<NumProperty> getMyProperties(){
-        return myPropertiesList;
-    }
+//    public List<NumProperty> getMyProperties(){
+//        return myPropertiesList;
+//    }
 
 
 
