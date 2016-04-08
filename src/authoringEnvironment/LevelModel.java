a9d0066237debaa2ce9da3937e756e5b3ab5c4eb
David Yan <davidyanduke@gmail.com>
2016-04-07 22:11:29 -0400

    save levels works

---
+++ src/authoringEnvironment/LevelModel.java
@@ -0,0 +1,21 @@
+package authoringEnvironment;
+
+/**
+ * Created by davidyan on 4/7/16.
+ */
+import java.util.*;
+public class LevelModel {
+
+
+    private Map<ViewSprite,Model> myMap;
+
+    public LevelModel(Map<ViewSprite, Model> useMap){
+        myMap = useMap;
+    }
+
+    public Map<ViewSprite, Model> getMyMap() {
+        return myMap;
+    }
+
+
+}
