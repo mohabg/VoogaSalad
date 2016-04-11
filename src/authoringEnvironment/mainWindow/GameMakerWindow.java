0480420bc7a30b51d88ca60408eb4c8a12c9388f
Joe Jacob <joejacob95@gmail.com>
2016-04-11 01:27:23 -0400

    barely refactored GameMakerWindow. pretty good class; i just need to remove the settingswindow dependency in init()

--- src/authoringEnvironment/mainWindow/GameMakerWindow.java
+++ src/authoringEnvironment/mainWindow/GameMakerWindow.java
@@ -34,39 +34,37 @@ public class GameMakerWindow {
 
 	public GameMakerWindow() {
 		myTabPane = new TabPane();
-
 	}
 
 	public void init(SettingsWindow window) {
 		myWindow = window;
 		addNewTab();
-
-	}
-
-	public void createNewTab(Map<ViewSprite, Model> mySpriteMap) {
-		GameTab myTab = new GameTab(ItemWindowData.TAB + (myTabPane.getTabs().size() + 1), mySpriteMap, myWindow);
-
-		myTabPane.getTabs().add(myTab);
-		myTabPane.getSelectionModel().select(myTab);
 	}
 
 	public void addNewTab() {
 		createNewTab(new HashMap<ViewSprite, Model>());
 	}
 
-	public GameTab getCurrentTab() {
-		return (GameTab) myTabPane.getSelectionModel().getSelectedItem();
-	}
-
 	public void populateEditingFromSave(List<LevelModel> gameLevels) {
 		myTabPane.getTabs().clear();
 		for (LevelModel lm : gameLevels) {
-
 			createNewTab(lm.getMyMap());
 		}
 
 	}
+	
+	private void createNewTab(Map<ViewSprite, Model> mySpriteMap) {
+		String tabName = ItemWindowData.TAB + (myTabPane.getTabs().size() + 1);
+		GameTab myTab = new GameTab(tabName, mySpriteMap, myWindow);
 
+		myTabPane.getTabs().add(myTab);
+		myTabPane.getSelectionModel().select(myTab);
+	}
+	
+	public GameTab getCurrentTab() {
+		return (GameTab) myTabPane.getSelectionModel().getSelectedItem();
+	}
+	
 	public TabPane getMyTabPane() {
 		return myTabPane;
 	}
