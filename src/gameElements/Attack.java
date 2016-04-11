d06dac351049cdbac15e5f30cd6a4a408906ccac
srikarpyda <sp285@duke.edu>
2016-04-10 22:24:37 -0400

    modified attack

--- src/gameElements/Attack.java
+++ src/gameElements/Attack.java
@@ -3,12 +3,29 @@ package gameElements;
 public abstract class Attack extends Sprite implements Behavior{
 		
 		private Movement movement;
-		
+		private double damage;
+		public Attack(){
+			
+		}
+		public Attack(Movement movement){
+			this.movement = movement;
+			damage = 0;
+		}
+		public Attack(Movement movement, double damage){
+			this.movement = movement;
+			this.damage = damage;
+		}
 		public Movement getMovement() {
 			return movement;
 		}
 		public void setMovement(Movement movement) {
 			this.movement = movement;
+		}
+		public double getDamage() {
+			return damage;
+		}
+		public void setDamage(double damage) {
+			this.damage = damage;
 		}
 		
 		
