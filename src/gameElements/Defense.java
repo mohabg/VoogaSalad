e96f837378852344924e4b841a7d822e03aa851f
jessepollack <jsr43@duke.edu>
2016-04-12 16:54:09 -0400

    filling in pseudocode

--- src/gameElements/Defense.java
+++ src/gameElements/Defense.java
@@ -2,14 +2,23 @@ package gameElements;
 
 public abstract class Defense extends Sprite implements Behavior{
 	private Health myHealth;
+	private ApplyBehaviorConditions behaviorConditions;
 	
-	public Defense(){
-		myHealth=new Health();
-	}
 	public Defense(Health health){
 		myHealth=health;
+		behaviorConditions = new ApplyBehaviorConditions();
 	}
 	
+	public boolean readytoDefend(Sprite sprite){
+	behaviorConditions.setCurrentDuration(behaviorConditions.getCurrentDuration()-1);
+	if (behaviorConditions.getCurrentDuration() <= 0){
+	if (Math.random() < behaviorConditions.getShootingProbability()){
+	if (behaviorConditions.getMaxDuration() > 0){ 
+		return true;}
+	}
+	}
+	return false;
+	}
 	public Health getHealth() {
 		return this.myHealth;
 	}
