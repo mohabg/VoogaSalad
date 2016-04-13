31c4786ff22686fae16cbd4affd9a91440414722
GAURAV KUMAR <gaurav.s.kumar3@gmail.com>
2016-04-13 03:47:51 -0400

    make game loop

---
+++ src/gameElements/GameEditor.java
@@ -0,0 +1,70 @@
+package gameElements;
+
+public class GameEditor implements IGameEditor {
+	
+	private Game myGame;
+
+	public GameEditor() {
+		myGame = new Game();
+	}
+	
+	public Game getGame() {
+		return myGame;
+	}
+	
+	public void setGame(Game newGame) {
+		myGame = newGame;
+	}
+	
+	public GameInfo getGameInfo() {
+		return myGame.getGameInfo();
+	}
+	
+	public void setGameInfo(GameInfo newInfo) {
+		myGame.setGameInfo(newInfo);
+	}
+	
+	public void addLevel(int index, LevelProperties levelProperties) {
+		myGame.createLevel(index, levelProperties);
+	}
+	
+	public void setCurrentLevel(int index){
+		myGame.setCurrentLevel(index);
+	}
+
+	public Level getCurrentLevel(){
+		return myGame.getCurrentLevel();
+	}
+	
+/*	public int addGoal(GoalProperties newGoal) {
+		return myGame.getCurrentLevel().setGoal(newGoal);
+	}
+
+	public void updateGoal(Integer goalID, GoalProperties newGoal) {
+	    myGame.getCurrentLevel().updateGoal(goalID, newGoal);
+	}
+*/
+	public void deleteGoal(Integer goalID) {
+	    myGame.getCurrentLevel().deleteGoal(goalID);
+	}
+	
+	public void setLevelCharacteristics(LevelProperties levelProperties) {  
+		myGame.getCurrentLevel().setLevelProperties(levelProperties);
+	}
+	
+	public Integer getUserSprite() {
+		return myGame.getCurrentLevel().getCurrentSpriteID();
+	}
+
+	public void setUserSprite(Integer spriteID) {
+		myGame.getCurrentLevel().setCurrentSpriteID(spriteID);
+	}
+	
+	public void step() {
+		myGame.getCurrentLevel().update();
+	}
+	
+	public void updateGame() {
+    	myGame.getCurrentLevel().update();
+    }
+}
