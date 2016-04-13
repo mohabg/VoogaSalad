9877d4aa491dafd1332cba9111fc2caff4503fcc
HUIJIA YU <hy91@duke.edu>
2016-04-12 21:44:38 -0400

    finally binding

--- src/authoringEnvironment/settingsWindow/VisualFactory.java
+++ src/authoringEnvironment/settingsWindow/VisualFactory.java
@@ -20,118 +20,119 @@ import java.util.List;
  * @author David Yan, Joe Jacob, Huijia Yu
  */
 public class VisualFactory {
-    private Settings mySettings;
-    private ResourcesReader myReader;
+	private Settings mySettings;
+	private ResourcesReader myReader;
 
-    public VisualFactory(){
-        mySettings = new Settings();
-    }
+	public VisualFactory() {
+		mySettings = new Settings();
+	}
 
-    public String reflectionTest(Sprite mySprite){
+	public String reflectionTest(Sprite mySprite) {
 
-        return "";
-    }
+		return "";
+	}
 
-    public static void main(String[] args){
+	public static void main(String[] args) {
 
+	}
 
+	public Constructor[] getConstructors(Class myClass) {
+		return myClass.getConstructors();
+	}
 
-    }
+	// public HBox makeNewSpinner(Parameter myParam){
+	// HBox myBox = new HBox(8);
+	// myBox.setPadding(new Insets(20,20,20,20));
+	// Label myLabel = new Label(myParam.getName());
+	// Spinner mySpinner = new Spinner();
+	// SpinnerValueFactory factory = new
+	// SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, 0);
+	// mySpinner.setValueFactory(factory);
+	// mySpinner.setEditable(true);
+	// //TODO: Possibly use param to bind spinner to?
+	// factory.valueProperty().bindBidirectional(mySprite.getMySpriteProperties().getMyY());
+	//
+	//
+	// myBox.getChildren().addAll(myLabel,mySpinner);
+	// return myBox;
+	//
+	//
+	// Spinner mySpinner2 = new Spinner();
+	// SpinnerValueFactory factory2 = new
+	// SpinnerValueFactory.DoubleSpinnerValueFactory(0, 10000, 0 );
+	// mySpinner2.setValueFactory(factory2);
+	// mySpinner2.setEditable(true);
+	//
+	//
+	//
+	// }
 
-    public Constructor[] getConstructors(Class myClass){
-        return myClass.getConstructors();
-    }
+	public static HBox makeTextInputBox(Parameter myParam, Field f) {
+		Label label1 = new Label("This:");
+		TextField textField = new TextField(f.toString());
+		HBox hb = new HBox();
+		hb.getChildren().addAll(label1, textField);
+		hb.setSpacing(10);
+		// TODO: Bind this to parameter if possible
+		return hb;
+	}
 
-//    public HBox makeNewSpinner(Parameter myParam){
-//        HBox myBox = new HBox(8);
-//        myBox.setPadding(new Insets(20,20,20,20));
-//        Label myLabel = new Label(myParam.getName());
-//        Spinner mySpinner = new Spinner();
-//        SpinnerValueFactory factory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, 0);
-//        mySpinner.setValueFactory(factory);
-//        mySpinner.setEditable(true);
-//        //TODO: Possibly use param to bind spinner to?
-//        factory.valueProperty().bindBidirectional(mySprite.getMySpriteProperties().getMyY());
-//
-//        
-//        myBox.getChildren().addAll(myLabel,mySpinner);
-//        return myBox;
-//        
-//        
-//        Spinner mySpinner2 = new Spinner();
-//        SpinnerValueFactory factory2 = new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 10000, 0 );
-//        mySpinner2.setValueFactory(factory2);
-//        mySpinner2.setEditable(true);
-//
-//
-//        
-//    }
+	private <T> List<T> makeList(Class<T> type) {
+		List<T> pp = new ArrayList<T>();
+		System.out.println(pp.getClass().toGenericString());
+		return new ArrayList<T>();
+	}
 
-    public static HBox makeTextInputBox(Parameter myParam, Field f){
-        Label label1 = new Label("This:");
-        TextField textField = new TextField(f.toString());
-        HBox hb = new HBox();
-        hb.getChildren().addAll(label1, textField);
-        hb.setSpacing(10);
-        //TODO: Bind this to parameter if possible
-        return hb;
-    }
-
-
-    private <T> List<T> makeList(Class<T> type) {
-    	List<T> pp = new ArrayList<T>();
-    	System.out.println(pp.getClass().toGenericString());
-    	return new ArrayList<T>();
-    }
-    //TODO: Binding and figuring out list of objects in reflection
-    public TabPane getMyTabs (Sprite mySprite){
-        TabPane myTabs = new TabPane();
+	// TODO: Binding and figuring out list of objects in reflection
+	public TabPane getMyTabs(Sprite mySprite) {
+		TabPane myTabs = new TabPane();
 
 		Field[] fields = mySprite.getClass().getDeclaredFields();
-		for (Field f: fields){
-			myTabs.getTabs().add(getOneTab(f));
+
+		for (Field f : fields) {
+			myTabs.getTabs().add(getOneTab(f, mySprite));
+
 		}
-//       
-        return myTabs;
-    }
+		//
+		return myTabs;
+	}
 
-	private Tab getOneTab(Field f) {
-		Tab myTab = new Tab("TESTING");
-        VBox myBox = new VBox();
-        AnchorPane myPane = new AnchorPane();
-        
-			Field[] properties = f.getType().getDeclaredFields();
-			for (Field p: properties){
-		        Spinner mySpinner = new Spinner();
-		        SpinnerValueFactory factory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 10000, 0 );
-		        mySpinner.setValueFactory(factory);
-		        mySpinner.setEditable(true);
-				System.out.println("+++++++"+p.getType().toGenericString());
+	private Tab getOneTab(Field f, Sprite mySprite) {
+		Tab myTab = new Tab(f.getName());
+		VBox myBox = new VBox();
+		AnchorPane myPane = new AnchorPane();
+		myBox.getChildren().add(oneSpinner(f, mySprite));
+		// Field[] properties = f.getType().getDeclaredFields();
+		// for (Field p: properties){
+		// myBox.getChildren().add(oneSpinner(p));
+		// }
 
-//		        p.getType().asSubclass(Property.class)
-		        try {
-					factory.valueProperty().bindBidirectional((Property) p.get(p.getType().newInstance()));
-				} catch (IllegalArgumentException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InstantiationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-		        myBox.getChildren().add(mySpinner);
-
-			}
-		
-		
-		
-        myPane.getChildren().add(myBox);
-        myTab.setContent(myPane);
+		myPane.getChildren().add(myBox);
+		myTab.setContent(myPane);
 		return myTab;
 	}
 
+	private Spinner oneSpinner(Field p, Sprite mySprite) {
+
+		p.setAccessible(true);
+		Spinner mySpinner = new Spinner();
+		SpinnerValueFactory factory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 10000, 0);
+		mySpinner.setValueFactory(factory);
+		mySpinner.setEditable(true);
+		System.out.println("+++++++" + p.getType().toGenericString());
+
+		// p.getType().asSubclass(Property.class)
+//			System.out.println("----" + p.getType().toGenericString());
+
+			try {
+				Property thisproperty = (Property) p.get(mySprite);
+				factory.valueProperty().bindBidirectional(thisproperty);
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		
+		return mySpinner;
+
+	}
 
 }
