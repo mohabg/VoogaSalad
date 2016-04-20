ea7a71ecc3a2327f7f19805155454114001daa0f
Joe Jacob <joejacob95@gmail.com>
2016-04-20 13:33:39 -0400

    xml seems to be working but getting a null ptr exception

--- src/XStreamHandlers/FXConverters.java
+++ src/XStreamHandlers/FXConverters.java
@@ -1,6 +1,15 @@
 package XStreamHandlers;
 
+import java.util.List;
+import java.util.Map;
+
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.Converter;
+
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.MapProperty;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 
 /**
  * Utility to configure a xStream with JavaFX property converters.<br>
@@ -18,12 +27,19 @@ public class FXConverters {
         xStream.registerConverter(new DoublePropertyConverter(xStream.getMapper()));
         xStream.registerConverter(new LongPropertyConverter(xStream.getMapper()));
         xStream.registerConverter(new IntegerPropertyConverter(xStream.getMapper()));
-        xStream.registerConverter(new ObservableListConverter(xStream.getMapper()));
-        xStream.registerConverter(new ObservableMapConverter(xStream.getMapper()));
+        //xStream.registerConverter(new ObservableListConverter(xStream.getMapper()));
+        //xStream.registerConverter(new ObservableMapConverter(xStream.getMapper()));
         xStream.registerConverter(new ListPropertyConverter(xStream.getMapper()));
         xStream.registerConverter(new MapPropertyConverter(xStream.getMapper()));
+        xStream.registerConverter(new ConverterWrapper(lookupTypeConverter(xStream, Map.class), ObservableMap.class));
+        xStream.registerConverter(new ConverterWrapper(lookupTypeConverter(xStream, List.class), ObservableList.class));
+        xStream.registerConverter(new ConverterWrapper(lookupTypeConverter(xStream, Map.class), MapProperty.class));
+        xStream.registerConverter(new ConverterWrapper(lookupTypeConverter(xStream, List.class), ListProperty.class));
         xStream.registerConverter(new ViewSpriteConverter());
     }
-
+    
+    private static Converter lookupTypeConverter(XStream xStream, Class clazz) {
+    	          return xStream.getConverterLookup().lookupConverterForType(clazz);
+    	    }
 
 }
