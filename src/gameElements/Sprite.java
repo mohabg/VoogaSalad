5c92931ae3a31f4827f27533bbea2c1e84b95277
David Yan <davidyanduke@gmail.com>
2016-04-21 17:06:39 -0400

    uncomment collisions

--- src/gameElements/Sprite.java
+++ src/gameElements/Sprite.java
@@ -1,45 +1,20 @@
 package gameElements;
 
 import authoringEnvironment.RefObject;
-import behaviors.Attack;
-import behaviors.Behavior;
-import behaviors.Bullet;
-import behaviors.MoveHorizontally;
-import behaviors.MoveVertically;
-import behaviors.SquarePattern;
+import behaviors.*;
 import collisions.ActorCollision;
 import collisions.Collision;
-import collisions.DamageCollision;
 import collisions.EnemyCollision;
 import gameplayer.SpriteFactory;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.MapProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.property.SimpleMapProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import authoringEnvironment.RefObject;
+import java.util.*;
 
 /**
  * Anything that is on the screen is described by this class. Has
@@ -101,7 +76,7 @@ public class Sprite {
 		canMove = new SimpleBooleanProperty(true);
 		myHealth = new Health(100);
 
-		//myCollisions.add(new EnemyCollision());
+		myCollisions.add(new EnemyCollision());
 
 		Attack bullet = new Bullet();
 		userPressBehaviors.put(KeyCode.SPACE, bullet);
