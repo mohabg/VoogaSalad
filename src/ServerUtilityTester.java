6203a0eee6ff3c57156ab52662bf5fceb637e7dc
David Yan <davidyanduke@gmail.com>
2016-04-25 13:31:23 -0400

    prepare to hopefully integrate server utility

--- src/ServerUtilityTester.java
+++ src/ServerUtilityTester.java
@@ -1,23 +1,23 @@
-//import authoringEnvironment.ServerUtility;
-//import com.jcraft.jsch.SftpException;
-//
-//import java.io.IOException;
-//
-///**
-// * Created by davidyan on 4/18/16.
-// */
-//public class ServerUtilityTester {
-//
-//    public static void main(String[] args){
-//        ServerUtility myUtility = new ServerUtility();
-//        try {
-////            myUtility.addFile(new File("SavedGameData/SavedGames/jkjk.xml"));
-//            myUtility.getFile("jkjk");
-//        }  catch (SftpException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//}
+import authoringEnvironment.ServerUtility;
+import com.jcraft.jsch.SftpException;
+
+import java.io.IOException;
+
+/**
+ * Created by davidyan on 4/18/16.
+ */
+public class ServerUtilityTester {
+
+    public static void main(String[] args){
+        ServerUtility myUtility = new ServerUtility();
+        try {
+//            myUtility.addFile(new File("SavedGameData/SavedGames/jkjk.xml"));
+            myUtility.getFile("jkjk");
+        }  catch (SftpException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
