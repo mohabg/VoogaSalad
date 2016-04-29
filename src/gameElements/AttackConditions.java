44fe8c291f0e3dd2eb1d525e39922beb26f0ad6c
David Yan <davidyanduke@gmail.com>
2016-04-29 18:36:16 -0400

    simple attack ai based on probability

--- src/gameElements/AttackConditions.java
+++ src/gameElements/AttackConditions.java
@@ -1,9 +1,39 @@
 package gameElements;
 
+import java.awt.Point;
+import java.util.*;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
 public class AttackConditions extends ExecuteConditions{
+
+	private DoubleProperty myAttackProbability;
 	
 	public AttackConditions(){
 		super();
+		myAttackProbability = new SimpleDoubleProperty(0.5);
+	}
+	
+	public boolean isAIReady() {
+		if (Math.random() < getProbability()) {
+			double elapsedTime = System.currentTimeMillis() - this.getRunningTime();
+			if (elapsedTime >= this.getFrequency() * 1000) {
+				this.setRunningTime(System.currentTimeMillis());
+				if (getDistFromUser() >= getMinDistFromUser() && getDistFromUser() <= getMaxDistFromUser()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	
+	public boolean doesShoot(){
+		//use probability to return left right top
+		if(Math.random() >= myAttackProbability.doubleValue()){
+			return true;
+		}
+		return false;
 	}
 	
 }
